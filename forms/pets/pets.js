cd6a0218489df43facbb606d61b83a707fc583be
ethan2050 <ejm60549@creighton.edu>
2020-09-18 12:31:37 -0500

    Steps3A-D

---
+++ forms/pets/pets.js
@@ -0,0 +1,6 @@
+let petNames = ["Darcy","Jack","Jerry","Marty","Vito","Vinny","Riley","Saddie","Marge","Sam"]
+/*alert(petNames[1])*/
+let withMarmaduke = ["Darcy","Jack","Jerry","Marty","Vito","Vinny","Riley","Saddie","Marge","Sam"]
+withMarmaduke.push("Marmaduke")
+alert(withMarmaduke)
+ 
